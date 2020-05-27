958912257b7189a3007ce59b40692cb13e992e27
Yotam_d <44746421+Yotam124@users.noreply.github.com>
2020-05-27 23:52:26 +0300

    Animals Added
    Tiger and Eagle

---
+++ Assets/Scripts/DestinationChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestinationChange : MonoBehaviour
+{
+    [SerializeField] int xRange1 = 200;
+    [SerializeField] int xRange2 = 240;
+    [SerializeField] int zRange1 = 2800;
+    [SerializeField] int zRange2 = 2840;
+
+    public int xPos;
+    public int zPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Animal")
+        {
+            Debug.Log("Colide!!");
+            xPos = Random.Range(xRange1, xRange2);
+            zPos = Random.Range(zRange1, zRange2);
+            this.gameObject.transform.position = new Vector3(xPos, this.transform.position.y, zPos);
+        }
+    }
+}
