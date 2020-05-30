3ed769926f3d3c3af876f30c2c7305c48aaa87d7
Yotam_d <44746421+Yotam124@users.noreply.github.com>
2020-05-30 19:55:33 +0300

    Player Upgrades
    Mouse Movement
    Weapons Swap
    Weapons Shooting
    Weapons Animations

---
+++ Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private WeaponManager weaponManager;
+
+    public float fireRate = 15f;
+    private float nextTimeToFire;
+    public float damage = 20f;
+
+    private void Awake()
+    {
+        weaponManager = GetComponent<WeaponManager>();    
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        WeaponShoot();
+    }
+
+    void WeaponShoot()
+    {
+        // Assualt Rifle
+        if (weaponManager.GetCurrentSelectedWeapon().fireType == WeaponFireType.MULTIPLE)
+        {
+            if (Input.GetMouseButton(0) && Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+
+                weaponManager.GetCurrentSelectedWeapon().ShootAnimation();
+            }
+        }
+        //Weapons that shoots SINGLE
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //Handle Axe
+                if (weaponManager.GetCurrentSelectedWeapon().tag == Tags.AXE_TAG)
+                {
+                    weaponManager.GetCurrentSelectedWeapon().ShootAnimation();
+                }
+
+                //Handle shoot
+                if (weaponManager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET)
+                {
+                    weaponManager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                    //BulletFired();
+                }
+                //Arrow or Spear
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Weapons Scripts/WeaponHandler.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponAim
+{
+    NONE,
+    SELF_AIM,
+    AIM
+}
+
+public enum WeaponFireType
+{
+    SINGLE,
+    MULTIPLE
+}
+
+public enum WeaponBulletType
+{
+    NONE,
+    BULLET,
+    ARROW,
+    SPEAR
+}
+
+
+public class WeaponHandler : MonoBehaviour
+{
+    private Animator anim;
+
+    public WeaponAim weaponAim;
+
+    [SerializeField]
+    private GameObject muzzleFlash;
+
+    [SerializeField]
+    private AudioSource shootSound, reloadSound;
+
+    public WeaponFireType fireType;
+
+    public WeaponBulletType bulletType;
+
+    public GameObject attackPoint;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();    
+    }
+   
+    public void ShootAnimation()
+    {
+        anim.SetTrigger(AnimationTags.SHOOT_TRIGGER);
+    }
+
+    public void Aim(bool canAim)
+    {
+        anim.SetBool(AnimationTags.AIM_PARAMETER, canAim);
+    }
+
+    void Turn_On_MuzzleFlash()
+    {
+        muzzleFlash.SetActive(true);
+    }
+
+    void Turn_Off_MuzzleFlash()
+    {
+        muzzleFlash.SetActive(false);
+    }
+
+    void Play_ShootSound()
+    {
+        shootSound.Play();
+    }
+
+    void Play_ReloadSound()
+    {
+        reloadSound.Play();
+    }
+
+    void Turn_On_AttackPoint()
+    {
+        attackPoint.SetActive(true);
+    }
+    void Turn_off_AttackPoint()
+    {
+        if (attackPoint.activeInHierarchy)
+        {
+            attackPoint.SetActive(false);
+        }
+    }
+
+
+
+}
